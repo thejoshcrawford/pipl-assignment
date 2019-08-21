1970ed304c211849c4aa34133feba153acdd2726
Josh Crawford <josh@crawford.cc>
2019-08-21 07:17:21 -0700

    Added Question, Answer and BrowserInstance models

--- src/polls_api/admin.py
+++ src/polls_api/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Poll
+from .models import Question
+from .models import Answer
+from .models import BrowserInstance
 
 admin.site.register(Poll)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(BrowserInstance)
--- src/polls_api/models.py
+++ src/polls_api/models.py
@@ -4,4 +4,17 @@ from django.db import models
 class Poll(models.Model):
     name = models.TextField()
     description = models.TextField()
+    url = models.TextField()
+
+class Question(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    name = models.TextField()
+
+class BrowserInstance(models.Model):
+    ip = models.TextField()
+    user_agent = models.TextField()
+
+class Answer (models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    browser_instance = models.ForeignKey(BrowserInstance, on_delete=models.CASCADE)
 

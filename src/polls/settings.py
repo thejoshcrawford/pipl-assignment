5a80d14d87f3ff82766d9a6c7219d8fed7b8ad66
Josh Crawford <josh@crawford.cc>
2019-08-23 21:36:46 -0700

    refactor vue app to js file

--- src/polls/settings.py
+++ src/polls/settings.py
@@ -122,8 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 
 # Hashfield settings
 HASHID_FIELD_SALT = "f95Imz7gzTYACS3uzdHK9QXho6NIwQqJ621OarXnUPbRqNEdlKibjbRV6uuzN3QJDonM6ckQuqFpZSDY"
